f11309e87d80276316d3616fa510c855e4ffbfe3
Joel Lazernie A. Aguzar <23-00562@g.batstate-u.edu.ph>
2024-05-07 09:33:31 +0800

    Add files via upload

---
+++ Final_Lab_Exam_CS121_Aguzar_Alcaraz/Utils/dice_game.py
@@ -0,0 +1,53 @@
+from random import randint
+from user_manager import *
+
+class DiceGame:
+    def __init__ (self, username):
+        pass
+
+    def load_score():
+        pass
+
+    def save_scores():
+        pass
+
+    def play_game(self, cpu, username):
+        while True:
+            try:
+                print ("Starting Dice Game...")
+                win = 0
+                for i in range (3):
+                    cpu = randint(1, 6)
+                    user = randint(1, 6)
+                    print()
+                    print (f"cpu: {cpu}")
+                    print (f"user: {user}")
+                    if user > cpu:
+                        win += 1
+                        print ("You win! You earned 1 point")
+                    elif user < cpu:
+                        print ("You lose! You didn't earn a point")
+                    else:
+                        print ("It's a tie")
+
+                print (win)
+
+                if win != 3:
+                    print("You cannot proceed to the next stage")
+                else:
+                    print("You can proceed to the next stage")
+                    choice = input("Would you like to continue?[y/or any character]: ").lower()
+                    if choice == "y":
+                        pass
+                    else:
+                        pass
+            except ValueError as e:
+                
+    def show_top_scores():
+        pass
+
+    def logout():
+        pass
+    
+    def menu():
+        pass
